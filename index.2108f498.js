024abd5f40b0543167344bd3f16ef4e19cedcc57
Eduard Herasym <edkido29@gmail.com>
2024-03-26 17:16:47 +0200

    make build

---
+++ index.2108f498.js
@@ -0,0 +1,2 @@
+"#menu"===window.location.hash&&document.body.classList.add("page__body--with-menu"),window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.querySelector("#form");e.addEventListener("submit",function(n){n.preventDefault(),e.reset()},!1);
+//# sourceMappingURL=index.2108f498.js.map
